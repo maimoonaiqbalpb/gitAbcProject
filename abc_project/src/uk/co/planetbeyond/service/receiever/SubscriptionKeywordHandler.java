43e7d65be1aecd1572a3a2a2c8441a7fcbc68df3
planetbeyond <planetbeyond@planetbeyond-HP-ZBook-14-G2>
2017-05-17 15:30:21 +0500

    Functionality added to SubscriptionKeywordHandler.

--- abc_project/src/uk/co/planetbeyond/service/receiever/SubscriptionKeywordHandler.java
+++ abc_project/src/uk/co/planetbeyond/service/receiever/SubscriptionKeywordHandler.java
@@ -2,5 +2,8 @@ package uk.co.planetbeyond.service.receiever;
 
 public class SubscriptionKeywordHandler
 {
-
+	public static void main(String[] args)
+	{
+		System.out.println("Subscription Keyword Handler");
+	}
 }
